def28c63b92e4c859649f7f01add22968e803170
dragoshelemenet <sogardoc@gmail.com>
2026-04-10 11:50:34 +0000

    fix homepage hover previews for video media

--- wsod-site/src/components/home/ServiceFolders.tsx
+++ wsod-site/src/components/home/ServiceFolders.tsx
@@ -2,6 +2,22 @@ import Link from "next/link";
 import { homeCategories } from "@/lib/data/home-data";
 import { getHomeCategoryPreviewMap } from "@/lib/data/db-queries";
 
+function isImageUrl(url?: string | null) {
+  if (!url) return false;
+  const clean = url.split("?")[0].toLowerCase();
+  return [".jpg", ".jpeg", ".png", ".webp", ".gif", ".avif"].some((ext) =>
+    clean.endsWith(ext)
+  );
+}
+
+function isVideoUrl(url?: string | null) {
+  if (!url) return false;
+  const clean = url.split("?")[0].toLowerCase();
+  return [".mp4", ".webm", ".mov", ".m4v", ".ogg"].some((ext) =>
+    clean.endsWith(ext)
+  );
+}
+
 export default async function ServiceFolders() {
   const previewMap = await getHomeCategoryPreviewMap();
 
@@ -30,12 +46,17 @@ export default async function ServiceFolders() {
                       key={`${service.slug}-${index}-${src}`}
                       className={`folder-hover-shot folder-hover-shot-${index + 1}`}
                     >
-                      {/* eslint-disable-next-line @next/next/no-img-element */}
-                      <img
-                        src={src}
-                        alt=""
-                        loading="lazy"
-                      />
+                      {isImageUrl(src) ? (
+                        // eslint-disable-next-line @next/next/no-img-element
+                        <img src={src} alt="" loading="lazy" />
+                      ) : isVideoUrl(src) ? (
+                        <video
+                          src={src}
+                          muted
+                          playsInline
+                          preload="metadata"
+                        />
+                      ) : null}
                     </div>
                   ))}
                 </div>
