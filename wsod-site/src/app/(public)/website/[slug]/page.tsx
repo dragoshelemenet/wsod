283197db99539b642fedcb89dcea2b9f9f5df44f
dragoshelemenet <sogardoc@gmail.com>
2026-04-13 07:15:29 +0000

    Fix broken website page imports

--- wsod-site/src/app/(public)/website/[slug]/page.tsx
+++ wsod-site/src/app/(public)/website/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
-import ZoomableWebsitePreview from "@/components/public/ZoomableWebsitePreview";
-import {
 import WebsitePreviewZoom from "@/components/public/WebsitePreviewZoom";
+import {
   getPublishedMediaByCategory,
   getPublishedMediaBySlug,
 } from "@/lib/dashboard/queries";
